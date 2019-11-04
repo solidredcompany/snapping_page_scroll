7d0f30f1e02fab5bde7b1ed4a21732bf2f56b2e6
Hannes Hultergård <hannes.hultergard@gmail.com>
2019-11-04 21:10:53 +0100

    Adds example

---
+++ example/example.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:snapping_page_scroll/snapping_page_scroll.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+
+  Widget customCard(String text){
+    return Padding(
+      padding: EdgeInsets.fromLTRB(20, 100, 20, 100),
+      child: Card(
+        child: Text(text),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber,
+        appBar: AppBar(),
+        body: SnappingPageScroll(
+          viewportFraction: 0.75,
+          children: <Widget>[
+            customCard('Card 1'),
+            customCard('Card 2'),
+            customCard('Card 3'),
+            customCard('Card 4'),
+            customCard('Card 5'),
+            customCard('Card 6'),
+          ],
+        ),
+      ),
+    );
+  }
+}
