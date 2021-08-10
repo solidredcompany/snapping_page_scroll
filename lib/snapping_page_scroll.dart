75cf6512b47e562d38d3e86b3a26a2d4fc7c4a2f
Hannes Hultergård <hannes.hultergard@gotapps.se>
2021-08-10 17:55:38 +0200

    Edit comments to follow Effective Dart

--- lib/snapping_page_scroll.dart
+++ lib/snapping_page_scroll.dart
@@ -15,28 +15,28 @@ class SnappingPageScroll extends StatefulWidget {
     this.viewportFraction = 1,
   }) : super(key: key);
 
-  ///The pages that the widget will scroll between and snap to.
+  /// The pages that the widget will scroll between and snap to.
   final List<Widget> children;
 
-  ///Called when the page changes.
+  /// Called when the page changes.
   final ValueChanged<int>? onPageChanged;
 
-  ///Index of the page that is shown initially.
+  /// Index of the page that is shown initially.
   final int initialPage;
 
-  ///The axis on which the pages is scrolled along.
+  /// The axis on which the pages is scrolled along.
   final Axis scrollDirection;
 
-  ///Option to enable / disable page indicators.
+  /// Option to enable / disable page indicators.
   final bool showPageIndicator;
 
-  ///Widget to use as indicator for the page currently on screen.
+  /// Widget to use as indicator for the page currently on screen.
   final Widget? currentPageIndicator;
 
-  ///Widget to use as indicator for the pages that not on screen.
+  /// Widget to use as indicator for the pages that not on screen.
   final Widget? otherPageIndicator;
 
-  ///With of page, where 1 is 100% of the screen.
+  /// With of page, where 1 is 100% of the screen.
   final double viewportFraction;
 
   @override
@@ -75,29 +75,29 @@ class _SnappingPageScrollState extends State<SnappingPageScroll> {
   @override
   Widget build(BuildContext context) {
     return Listener(
-      ///Get pointer (finger) position.
+      // Get pointer (finger) position.
       onPointerMove: (PointerMoveEvent pos) {
-        ///Runs if the time since the last scroll is undefined or over 100 milliseconds.
+        // Runs if the time since the last scroll is undefined or over 100 milliseconds.
         if (time == null ||
             DateTime.now().millisecondsSinceEpoch - time! > 100) {
           time = DateTime.now().millisecondsSinceEpoch;
           position = pos.position.dx;
 
-          ///The fingers x-coordinate.
+          // The fingers x-coordinate.
         } else {
           ///Calculates scroll velocity.
           final double v = (position - pos.position.dx) /
               (DateTime.now().millisecondsSinceEpoch - time!);
 
-          ///If the scroll velocity is to low, the widget will scroll as a PageView widget with
-          ///pageSnapping turned on.
+          // If the scroll velocity is to low, the widget will scroll as a PageView widget with
+          // pageSnapping turned on.
           if ((v < -2 || v > 2) &&
               !(v == double.nan ||
                   v == double.infinity ||
                   v == double.negativeInfinity)) {
-            ///Scrolls to a certain page based on the scroll velocity
-            //The velocity coefficient (v * velocity coefficient) can be increased to scroll faster,
-            //and thus further before snapping.
+            // Scrolls to a certain page based on the scroll velocity
+            // The velocity coefficient (v * velocity coefficient) can be increased to scroll faster,
+            // and thus further before snapping.
             _pageController!.animateToPage(_currentPage + (v * 1.2).round(),
                 duration: const Duration(milliseconds: 800),
                 curve: Curves.easeOutCubic);
@@ -115,15 +115,15 @@ class _SnappingPageScrollState extends State<SnappingPageScroll> {
                   _currentPage = page;
                 });
 
-                ///onPageChanged will pass the current page to the widget if that parameter is used
+                // onPageChanged will pass the current page to the widget if that parameter is used.
                 if (widget.onPageChanged != null) {
                   widget.onPageChanged!(page);
                 }
               },
-              //BouncingScrollPhysics can't be used since that will scroll to far because of the animation
+              // BouncingScrollPhysics can't be used since that will scroll to far because of the animation.
               physics: const ClampingScrollPhysics(),
 
-              ///Scroll direction will default to horizontal unless otherwise is specified
+              // Scroll direction will default to horizontal unless otherwise is specified.
               scrollDirection: widget.scrollDirection,
               children: widget.children,
             ),
@@ -133,7 +133,7 @@ class _SnappingPageScrollState extends State<SnappingPageScroll> {
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  ///Builds one indicator for every page
+                  // Builds one indicator for every page.
                   for (int i = 0; i < widget.children.length; i++)
                     i == _currentPage
                         ? (widget.currentPageIndicator ??
@@ -150,7 +150,7 @@ class _SnappingPageScrollState extends State<SnappingPageScroll> {
   }
 }
 
-/// Used to remove scroll glow
+/// Custom scroll behaivior to remove scroll glow.
 class CustomScroll extends ScrollBehavior {
   @override
   Widget buildViewportChrome(
